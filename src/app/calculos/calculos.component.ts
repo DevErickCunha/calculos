fc029ee277f0e8563eb7fd160d5b405805c7e806
Erick Cunha <erickcuna20@gmail.com>
2025-02-17 13:35:16 -0300

    feat:calculos restantes foram adicionados nos resultados exibidos

--- src/app/calculos/calculos.component.ts
+++ src/app/calculos/calculos.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { Form, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core'; 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -15,68 +15,79 @@ export class CalculosComponent {
   calculado = false
 
   constructor(public formBuilder: FormBuilder) {
-    this.creatForm()
+    this.createForm();
   }
 
-  creatForm(){
-  this.form= this.formBuilder.group({
-    SistemaProducao: ['',[Validators.required,Validators.minLength(3)]],
-    TaxaMortalidadePreDesmama: [0, [Validators.min(1)]],
-    BezerrosAs: [0, [Validators.min(1)]],  
-    TaxaNascMedia: [0, [Validators.min(1)]],
-    TxNascVacasMultiparas: [0, [Validators.min(1)]],
-    TotalTouroVendido: [0, [Validators.min(1)]],
-    QuantCabecasAnual: [0, [Validators.min(1)]],
-    TotalUAPonderado: [0, [Validators.min(1)]],
-    AreaPastagemPerene: [0, [Validators.min(1)]],
-    TotalUATrabalho: [0, [Validators.min(1)]],
-  });
-
+  createForm() {
+    this.form = this.formBuilder.group({
+      SistemaProducao: ['', [Validators.required, Validators.minLength(3)]],
+      TaxaMortalidadePreDesmama: [0, [Validators.min(1)]],
+      BezerrosAs: [0, [Validators.min(1)]],
+      TaxaNascMedia: [0, [Validators.min(1)]],
+      TxNascVacasMultiparas: [0, [Validators.min(1)]],
+      TotalTouroVendido: [0, [Validators.min(1)]],
+      QuantCabecasAnual: [0, [Validators.min(1)]],
+      TotalUAPonderado: [0, [Validators.min(1)]],
+      AreaPastagemPerene: [0, [Validators.min(1)]],
+      TotalUATrabalho: [0, [Validators.min(1)]],
+    });
   }
+
   readonly dialog = inject(MatDialog);
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      data: {AnimaisMortosPreDesmama: this.AnimaisMortosPreDesmama() },
+      data: {
+        AnimaisMortosPreDesmama: this.AnimaisMortosPreDesmama(),
+        IntervaloEntrePartos: this.IntervaloEntrePartos(),
+        TaxaNatalidadeMultiparas: this.TaxaNatalidadeMultiparas(),
+        TaxaReproducaoDescartesMatrizes: this.TaxaReproducaoDescartesMatrizes(),
+        TaxaLotacaoAreaPasto: this.TaxaLotacaoAreaPasto(),
+        TaxaLotacaoEmAreaTotal: this.TaxaLotacaoEmAreaTotal(),
+      },
     });
   }
 
   AnimaisMortosPreDesmama(): number {
     return parseFloat((this.form.value.TaxaMortalidadePreDesmama * this.form.value.BezerrosAs).toFixed(2));
   }
-
+  
   IntervaloEntrePartos(): number {
     return parseFloat((this.form.value.TaxaNascMedia === 0 ? 0 : (1 / this.form.value.TaxaNascMedia) * 12).toFixed(2));
   }
-
+  
   TaxaNatalidadeMultiparas(): number {
-    return  this.form.value.TxNascVacasMultiparas;
+    return parseFloat(this.form.value.TxNascVacasMultiparas.toFixed(2));
   }
-
+  
   TaxaReproducaoDescartesMatrizes(): number {
-    return this.form.value.QuantCabecasAnual === 0 ? 0 : this.form.value.TotalTouroVendido / this.form.value.QuantCabecasAnual;
+    return parseFloat((this.form.value.QuantCabecasAnual === 0 ? 0 : this.form.value.TotalTouroVendido / this.form.value.QuantCabecasAnual).toFixed(2));
   }
-
+  
   TaxaLotacaoAreaPasto(): number {
-    return this.form.value.AreaPastagemPerene === 0 ? 0 : (this.form.value.TotalUAPonderado + this.form.value.TotalUATrabalho) / this.form.value.AreaPastagemPerene;
+    return parseFloat((this.form.value.AreaPastagemPerene === 0 ? 0 : (this.form.value.TotalUAPonderado + this.form.value.TotalUATrabalho) / this.form.value.AreaPastagemPerene).toFixed(2));
   }
-
+  
   TaxaLotacaoEmAreaTotal(): number {
-    return this.form.value.AreaPastagemPerene === 0 ? 0 : (this.form.value.TotalUAPonderado + this.form.value.TotalUATrabalho) / this.form.value.AreaPastagemPerene;
+    return parseFloat((this.form.value.AreaPastagemPerene === 0 ? 0 : (this.form.value.TotalUAPonderado + this.form.value.TotalUATrabalho) / this.form.value.AreaPastagemPerene).toFixed(2));
   }
-
-
+  
 
   salvarValores() {
     if (this.form.invalid) {
       return;
     }
     this.calculado = true;
-}
+  }
 }
 
-interface   DialogData {
+interface DialogData {
   AnimaisMortosPreDesmama: number;
+  IntervaloEntrePartos: number;
+  TaxaNatalidadeMultiparas: number;
+  TaxaReproducaoDescartesMatrizes: number;
+  TaxaLotacaoAreaPasto: number;
+  TaxaLotacaoEmAreaTotal: number;
 }
 
 @Component({
@@ -85,7 +96,6 @@ interface   DialogData {
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    FormsModule,
     MatButtonModule,
     MatDialogTitle,
     MatDialogContent,
